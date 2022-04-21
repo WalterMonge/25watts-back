7288831d5e7433a07c57dd0beb5407c1df38b9ab
Walter Monge <walter.monge@hotmail.com>
2022-04-21 12:43:38 -0300

    Correccion mail

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'username' => env('MAIL_USERNAME', 'e063c0b42964c3'),
+            'password' => env('MAIL_PASSWORD', '43500b35f15587'),
             'timeout' => null,
             'auth_mode' => null,
         ],
